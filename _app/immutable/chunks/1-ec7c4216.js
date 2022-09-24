016b8dd6818c81439f687585f89a6be26fbda284
bassjoe <18555116+bassjoe@users.noreply.github.com>
2022-09-24 14:49:02 +0000

    Deploying to gh-pages from @ bassjoe/pwa4@06fea05b527f65bf3a30ea838a36c4cdb85ace43 🚀

---
+++ _app/immutable/chunks/1-ec7c4216.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-dc3dc98e.js";export{t as component};
--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,12 @@
 self.addEventListener("install", (e) => {
-  console.log("service worker installed");
+  e.waitUntil(
+    caches.open("pwa4").then((t) => t.addAll([
+      "."
+    ]))
+  );
 });
-self.addEventListener("activate", (e) => {
-  console.log("service worker activated");
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((t) => t || fetch(e.request))
+  );
 });
