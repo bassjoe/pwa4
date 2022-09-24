06fea05b527f65bf3a30ea838a36c4cdb85ace43
bassjoe <18555116+bassjoe@users.noreply.github.com>
2022-09-24 16:48:49 +0200

    cache assets

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,8 +1,23 @@
-self.addEventListener('install', (event) => {
-	console.log('service worker installed');
+self.addEventListener('install', event => {
+	event.waitUntil(
+		caches
+			.open('pwa4')
+			.then(cache => cache.addAll([
+				".",
+			]))
+	);
 });
 
-self.addEventListener('activate', (event) => {
+/*
+self.addEventListener('activate', event => {
 	console.log('service worker activated');
 });
+*/
 
+self.addEventListener('fetch', event => {
+	event.respondWith(
+		caches
+			.match(event.request)
+			.then(response => response || fetch(event.request))
+	);
+});
